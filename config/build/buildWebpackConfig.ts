6dd13f2cfb433aabd1104dbca61e463cd02fb463
admin <sivenskij@gmail.com>
2024-02-27 16:31:53 +0300

    Декомпозирована конфигурация webpack

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import path from 'path';
+import webpack from 'webpack';
+
+import { BuildOptions } from './types/config';
+import { buildLoader } from './buildLoaders';
+import { buildResolves } from './buildResolves';
+import { buildPlugin } from './buildPlugins';
+
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+	const { paths, mode } = options;
+	
+	return {
+		mode: mode,
+		entry: paths.entry,
+		module: {
+			rules: buildLoader(),
+		},
+		resolve: buildResolves(),
+		output: {
+			filename: '[name].[contenthash].js',
+			path: paths.build,
+			clean: true
+		},
+		plugins: buildPlugin(options)
+	}
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,34 +1,22 @@
 import path from 'path';
-import HTMLWebpackPlugin from'html-webpack-plugin';
 import webpack from 'webpack';
 
-const config: webpack.Configuration = {
-	mode : 'development',
-	entry : path.resolve(__dirname, 'src', 'index.ts'),
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildPaths } from "./config/build/types/config";
 
-	module : {
-		rules: [{
-			test: /\.tsx?$/,
-			use: 'ts-loader',
-			exclude: /node_modules/,
-		}],
-	},
-	resolve : {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
+const paths: BuildPaths = {
+	entry: path.resolve(__dirname, 'src', 'index.ts'),
+	build: path.resolve(__dirname, 'build'),
+	html: path.resolve(__dirname, 'public', 'index.html')
+};
 
-	output : {
-		filename : '[name].[contenthash].js',
-		path: path.resolve(__dirname, 'build'),
-		clean: true
-	},
+const mode = 'development';
+const isDev= mode == 'development';
 
-	plugins : [
-		new HTMLWebpackPlugin({
-			template : path.resolve(__dirname, 'public', 'index.html')
-		}),
-		new webpack.ProgressPlugin()
-	]
-}
+const config: webpack.Configuration = buildWebpackConfig({
+	mode: mode,
+	paths: paths,
+	isDev: isDev
+});
 
 export default config;
