f6156ad9e2c7ff22fcc46fb082c691f76c3b0d21
admin <sivenskij@gmail.com>
2024-03-04 20:55:06 +0300

    Добавлена библтотека react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Counter } from "./components/Counter";
+import { Link, Route, Routes } from 'react-router-dom';
+
 import './index.scss';
+import MainPage from './pages/MainPage/MainPage';
+import AboutPage from './pages/AboutPage/AboutPage';
 
 
 const App = () => {
 	return (
 		<div className="app">
-			some text
-			<Counter />
+			<Link to={'/'}>Главная</Link>
+			<Link to={'/about'}>О сайте</Link>
+			<Routes>
+				<Route path={'/'} element={<AboutPage />} />
+				<Route path={'/about'} element={<MainPage />} />
+			</Routes>
 		</div>
 	);
 }
