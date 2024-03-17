51ccfa98322076c8ed6364ac3f396e23e1d75546
admin <sivenskij@gmail.com>
2024-03-17 20:49:36 +0300

    Реализован Provider темы приложения

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React, { FC, useMemo, useState } from 'react';
+import { LOCAL_STORAGE_KEY_THEME, Theme, ThemeContext } from './ThemeContex';
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_KEY_THEME) as Theme || Theme.LIGHT;
+
+const ThemeProvider: FC = ({ children }) => {
+
+	const [ theme, setTheme ] = useState<Theme>(defaultTheme);
+
+	const defaultProps= useMemo(() => ({
+		theme: theme,
+		setTheme: setTheme
+	}), [ theme ]);
+
+	return (
+		<ThemeContext.Provider value={ defaultProps }>
+			{ children }
+		</ThemeContext.Provider>
+	)
+}
+
+export default ThemeProvider;
