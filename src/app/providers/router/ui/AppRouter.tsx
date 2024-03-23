a36aa3aa4930d22751b88fe9333924a37111b899
admin <sivenskij@gmail.com>
2024-03-23 23:20:44 +0300

    Роутинг приложения вынесен в папку app/provider/rout

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,25 @@
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import {routesConfig} from "shared/config/routeConfig/routeConfig";
+
+const AppRouter = () => {
+	return (
+		<Suspense fallback={<div>Загрузка...</div>}>
+			<Routes>
+				{ routesConfig.map(({ element, path }) => (
+					<Route
+						key={ path }
+						path={path}
+						element={(
+							<Suspense fallback={<div>Загрузка...</div>}>
+								{ element }
+							</Suspense>
+						)}
+					/>
+				))}
+			</Routes>
+		</Suspense>
+	)
+}
+
+export default AppRouter;
