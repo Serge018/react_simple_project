d667d2667c6e3f917a2a9a334866cef2ae3d194f
admin <sivenskij@gmail.com>
2024-04-07 22:35:30 +0300

    Добавлен плагин webpack.DefinePlugin для определения глобальной переменной __IS_DEV__

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -7,11 +7,13 @@ declare module '*.scss' {
 }
 
 
-declare module '*.png'
-declare module '*.jpeg'
-declare module '*.jpg'
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.jpg';
 declare module '*.svg' {
 	import React from 'react';
 	const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
 	export default SVG;
 }
+
+declare const __IS_DEV__ : boolean;
