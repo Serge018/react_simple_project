ef319dca4a0b9e28133b2088dda299e2ce430d5f
admin <sivenskij@gmail.com>
2024-05-01 21:18:39 +0300

    добавлена страница NotFoundPage для сценариев, когда в поисковой строке браузера введён некорректный URL

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
 	MAIN = 'main',
-	ABOUT = 'about'
+	ABOUT = 'about',
+	NOT_FOUND = 'not_found'
 }
 
 export const RoutePaths: Record<AppRoutes, string> = {
 	[ AppRoutes.MAIN ] : '/',
-	[ AppRoutes.ABOUT ] : '/about'
+	[ AppRoutes.ABOUT ] : '/about',
+	[ AppRoutes.NOT_FOUND ] : '*',
 }
 
 export const routesConfig: RouteProps[] = [
@@ -20,5 +23,9 @@ export const routesConfig: RouteProps[] = [
 	{
 		path : RoutePaths.about,
 		element: <AboutPage />
+	},
+	{
+		path : RoutePaths.not_found,
+		element: <NotFoundPage />
 	}
 ]
