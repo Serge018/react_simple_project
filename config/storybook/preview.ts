eaa0182ee2edd4d75fe597acd979c108f904ad6e
admin <sivenskij@gmail.com>
2024-05-12 00:24:52 +0300

    В конфигурацию preview stories внесены декораторы темы и роутинга

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,12 +1,14 @@
 import type { Preview } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
 
 const preview: Preview = {
 	decorators : [
 		StyleDecorator,
-		ThemeDecorator(Theme.LIGHT)
+		ThemeDecorator(Theme.LIGHT),
+		RouterDecorator
 	],
 	parameters: {
 		layout: 'fullscreen',
